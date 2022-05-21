820592c03df0898844ae22a02b13fa75ba34df22
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-05-21 22:16:50 +0300

    add knex config

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { Model } from 'objection';
+
 
 dotenv.config();
-
 const app: Express = express();
 const port = process.env.PORT;
+const knex = require('knex')({
+  client: 'postgres',
+  connection: async () => {
+    return {
+      host : process.env.PGHOST,
+      port : process.env.PGPORT,
+      user : process.env.PGUSER,
+      password : process.env.PGPASSWORD,
+      database : process.env.PGDATABASE,
+    };
+  }
+});
+Model.knex(knex);
 
 app.get('/', (req: Request, res: Response) => {
   console.log(req.url);
