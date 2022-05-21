06f62bd528fa431ea948d4de8332688603b94961
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-05-21 16:40:26 +0300

    init node

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  console.log(req.url);
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
