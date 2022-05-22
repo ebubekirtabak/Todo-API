83402a884c24d744df6b1264ef9e48db6dc1a9fc
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2022-05-22 19:41:03 +0300

    add secretKey as function parameter

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,11 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-  generateUserToken: (user: any, expiresIn: string = '30d'): string => {
+  generateUserToken: (
+    user: any,
+    expiresIn: string = '30d',
+    secretKey: string = process.env.JWT_SECRET_KEY as string
+  ): string => {
     const { id, email } = user;
     const token = jwt.sign(
       { user_id: id, email },
-      process.env.JWT_SECRET_KEY,
+      secretKey,
       {
         expiresIn,
       }
